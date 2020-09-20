13d556c7b86b70c908dfa1778ba7eb5270b6a2ce
zhouyang <zyzhouyang1@outlook.com>
2020-09-20 21:59:09 +0800

    添加窗口拖拽和窗口关闭事件

--- StickyNotes/StickyNotes.cpp
+++ StickyNotes/StickyNotes.cpp
@@ -1,6 +1,7 @@
 #include "StickyNotes.h"
 #include <QPushButton>
 #include <QIcon>
+#include <QMouseEvent>
 
 StickyNotes::StickyNotes(QWidget* parent)
         : QWidget(parent)
@@ -13,4 +14,39 @@ StickyNotes::StickyNotes(QWidget* parent)
         close->setIcon(ico);
         close->setIconSize(QSize(12, 12));
         close->setFlat(true);
+
+        connect(close, SIGNAL(clicked()), this, SLOT(close_click()));
+
+}
+
+void StickyNotes::close_click()
+{
+        this->close();
+}
+
+void StickyNotes::mousePressEvent(QMouseEvent* event)
+{
+        if (event->button() == Qt::LeftButton)
+        {
+                left_pressed = true;
+                start_point = event->globalPos();
+                windows_point = this->frameGeometry().topLeft();
+        }
+}
+
+void StickyNotes::mouseMoveEvent(QMouseEvent* event)
+{
+        if (event->buttons() & Qt::LeftButton )
+        {
+                QPoint pos = event->globalPos() - start_point;
+                this->move(windows_point + pos);
+        }
+}
+
+void StickyNotes::mouseReleaseEvent(QMouseEvent* event)
+{
+        if (event->button() == Qt::LeftButton)
+        {
+                left_pressed = false;
+        }
 }
--- StickyNotes/StickyNotes.h
+++ StickyNotes/StickyNotes.h
@@ -5,11 +5,21 @@
 
 class StickyNotes : public QWidget
 {
-    Q_OBJECT
+        Q_OBJECT
 
 public:
-    StickyNotes(QWidget *parent = Q_NULLPTR);
+        StickyNotes(QWidget* parent = Q_NULLPTR);
+
+protected:
+        void mousePressEvent(QMouseEvent* event);
+        void mouseMoveEvent(QMouseEvent* event);
+        void mouseReleaseEvent(QMouseEvent* event);
 
 private:
-    Ui::StickyNotesClass ui;
+        Ui::StickyNotesClass ui;
+        bool left_pressed = false;
+        QPoint start_point, windows_point;
+
+private slots:
+        void close_click();
 };
